23e2b6a98e485f8f1b5cefce73909e54d7f15dd1
PedroFR91 <pedromfr91@gmail.com>
2024-09-30 23:29:54 +0200

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,17 @@
+// 1 NODEJS - Enviroment to run JS outside Browser - Build on Chrome´s V8 JS Engine - Big Community - Full-Stack
+// 2 Browser VS NodeJS
+// DOM/NO DOM
+// window/no window
+// Interactive apps/server side apps
+// no filesystem/ filesystem
+// fragmentation/ versions
+// ES6 modules / commonJS
+// const amount = 8;
+
+if (amount < 9) {
+    console.log('small number')
+} else {
+    console.log('big number')
+}
+
+console.log('it´s my firts node app!')
---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+//CommonJS, every file is module (by default)
+// Modules - Encasulated Code (only share minimun)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavors')
+require('./7-mind-grenade')
+console.log(data)
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
